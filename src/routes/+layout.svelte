8095493c1aca360ccb2059393e009c6d17edbfa0
HaoChen <github@haochen.dev>
2025-02-25 23:42:06 +0800

    Add speed insights and analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import { Navigation } from '$lib/components/navigation';
 	import Seo from '$lib/components/seo.svelte';
 	import { StarryBackground } from '$lib/components/starry-background';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { ModeWatcher } from 'mode-watcher';
+
 	import '../app.css';
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
+
 	let { children } = $props();
 </script>
 
